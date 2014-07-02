621110b0abe8afc09b88596dda7d316360097849
maroslaw <marek.brodziak@gmail.com>
2014-07-02 22:00:16 +0200

    rename

---
+++ rainyday-old.js
@@ -0,0 +1,41 @@
+/**
+ * COLLISION function: default collision implementation
+ * @param drop one of the drops colliding
+ * @param collisions list of potential collisions
+ */
+RainyDay.prototype.COLLISION_SIMPLE = function(drop, collisions) {
+	var item = collisions;
+	var drop2;
+	while (item != null) {
+		var p = item.drop;
+		if (Math.sqrt(Math.pow(drop.x - p.x, 2) + Math.pow(drop.y - p.y, 2)) < (drop.r + p.r)) {
+			drop2 = p;
+			break;
+		}
+		item = item.next;
+	}
+
+	if (!drop2) {
+		return;
+	}
+
+	// rename so that we're dealing with low/high drops
+	var higher,
+		lower;
+	if (drop.y > drop2.y) {
+		higher = drop;
+		lower = drop2;
+	} else {
+		higher = drop2;
+		lower = drop;
+	}
+
+	this.clearDrop(lower);
+	// force stopping the second drop
+	this.clearDrop(higher, true);
+	this.matrix.remove(higher);
+	lower.draw();
+
+	lower.colliding = higher;
+	lower.collided = true;
+};
